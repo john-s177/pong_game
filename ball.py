5c19c594e2fd7858b0ec14abaf6031bb0a0a10cf
John Sameh <jsameh16@gmail.com>
2024-07-11 16:41:30 +0300

    Added scoreboard

---
+++ ball.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("slow")
+
+    def move(self):
+        self.goto(750,550)
